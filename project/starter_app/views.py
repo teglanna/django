5e3a39d39d085ffd3d6defa50a17ec2fb5da3d8f
teglanna <teglanna@gmail.com>
2016-06-15 19:34:00 +0200

    Added messages from json

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,11 +1,19 @@
+import json
+from config.settings.base import DJANGO_ROOT
 from django.shortcuts import render
 # from .models import Message
 
 
 def home(request):
-    # messages = Message.objects.order_by('order')
-    messages = ['valami1', 'valami2', 'nem']
-    context_dict = {
-        'messages': messages
-    }
-    return render(request, 'starter_app/home.html', context_dict)
+	# messages = Message.objects.order_by('order')
+	filepath = DJANGO_ROOT + '/content.JSON'
+
+	with open(filepath) as data_file:
+		content_data = json.load(data_file)
+	messages = content_data['mess']
+
+
+	context_dict = {
+		'messages': messages
+	}
+	return render(request, 'starter_app/home.html', context_dict)
